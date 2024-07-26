ed39f1ae0bc6c136ffeca47b795fe3731e9b25ce
Grace McDevitt <130437965+grcmcdvtt@users.noreply.github.com>
2024-07-26 16:36:40 -0400

    Update genBenignNamesToCheck.py

--- vm_experiment/reproduction/genBenignNamesToCheck.py
+++ vm_experiment/reproduction/genBenignNamesToCheck.py
@@ -2,10 +2,10 @@ import string
 import random
 
 def foo():
-	f1 = open("benignNamesE2.txt", "w")
+	f1 = open("benignNames.txt", "w")
 	for i in range(200):
-            for j in range(100):
-		reqBenign = 'b{}.benign.lan.'.format(j)    
-		f1.write("{} A\n".format(reqBenign))
+		for j in range(100):
+			reqBenign = 'b{}.benign.lan.'.format(j)    
+			f1.write("{} A\n".format(reqBenign))
 	f1.close()
 foo()
